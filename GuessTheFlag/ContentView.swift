ae70404ea6cb4ebbe620dbc51e7b4b979e40131f
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-03 14:32:33 -0300

    Finished Project

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,6 +10,21 @@ import SwiftUI
 struct FlagButton: View {
     let number: Int
     let image: String
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     let answer: Int
     var showAnimation = false
     let action: () -> ()
@@ -27,7 +42,7 @@ struct FlagButton: View {
                 .renderingMode(.original)
                 .clipShape(Capsule())
                 .shadow(radius: 5)
-                .accessibilityLabel(image, default: "Unknown value")
+                .accessibilityLabel(labels[image, default: "Unknown value"])
         }
         .rotation3DEffect(.degrees(number == answer ? self.animationAmount : 0), axis: (x: 0, y: 1, z: 0))
         .opacity(self.showAnimation && number != answer ? 0.25 : 1)
@@ -42,20 +57,6 @@ struct ContentView: View {
     @State private var userScore = 0
     
     @State private var Countries  = ["France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Spain", "USA"].shuffled()
-    
-    let labels = [
-        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
-        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
-        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
-        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
-        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
-        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
-        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
-        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
-        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
-        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
-        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
-    ]
     
     @State private var correctAnswer = Int.random(in: 0...2)
     
